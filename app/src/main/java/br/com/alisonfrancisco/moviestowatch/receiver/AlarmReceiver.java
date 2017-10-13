6331eacd98df04f6ceecdba32bfdeca4b11f4bf2
Alison Francisco <alisonflf@gmail.com>
2017-10-13 20:14:44 -0300

    Layout for tablets

--- app/src/main/java/br/com/alisonfrancisco/moviestowatch/receiver/AlarmReceiver.java
+++ app/src/main/java/br/com/alisonfrancisco/moviestowatch/receiver/AlarmReceiver.java
@@ -5,8 +5,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import br.com.alisonfrancisco.moviestowatch.MainActivity;
@@ -26,14 +24,10 @@ public class AlarmReceiver extends BroadcastReceiver {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
                 notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-
-        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
         NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(context).
                 setSmallIcon(R.drawable.ic_stat_reminder)
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(context.getString(R.string.notificationText))
-                .setSound(alarmSound)
                 .setAutoCancel(true).setWhen(when)
                 .setContentIntent(pendingIntent)
                 .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
